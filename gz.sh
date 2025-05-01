e55aa6aa3371841ae92fdc6d9f6d7d13492c1c19
Ruben <hilarioruben09@gmail.com>
2025-05-01 05:17:05 +0000

    Description and Control Pkgs

---
+++ gz.sh
@@ -0,0 +1,8 @@
+##!/bin/bash
+source /opt/ros/jazzy/setup.bash
+sudo apt-get update
+sudo apt-get install curl lsb-release gnupg
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install gz-harmonic
