a0bba62487530bf0c86d0e94803b8f4ccfe04ffc
Ruben <hilarioruben09@gmail.com>
2025-05-03 04:05:08 +0000

    models

--- src/robot_description/setup.py
+++ src/robot_description/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+import os
+from glob import glob as glob
 package_name = 'robot_description'
 
 setup(
@@ -7,6 +8,12 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/models', glob('models/*.sdf')),
+        ('share/' + package_name + '/models/rob_arm', glob('models/rob_arm/*.*')),
+        ('share/' + package_name + '/models/puzzlebot/meshes', glob('models/puzzlebot/meshes/*.*')),
+        ('share/' + package_name + '/models/puzzlebot/meshes', glob('models/puzzlebot/meshes/collision/*')),
+        ('share/' + package_name + '/models/puzzlebot/meshes', glob('models/puzzlebot/meshes/visual/*')),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
